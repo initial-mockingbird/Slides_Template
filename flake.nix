2641e6428ef04c21f5abfe33d715dcc44beab393
Dani :3 <daniel.andres.pinto@gmail.com>
2026-08-27 11:09:02 -0400

    icon

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    ghc-wasm.url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
 
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
@@ -11,7 +10,10 @@
       imports = [];
 
       perSystem = { self', pkgs, config, ... }: {
-        devShells.default = let tex = pkgs.texliveFull;
+        devShells.default = let 
+          tex = pkgs.texliveFull;
+          texmakerConf = import ./texmaker_config.nix;
+          texmakerConfFile = pkgs.writeText "texmaker.conf" texmakerConf.raw;
         in pkgs.mkShell {
           name = "latex-template";
           meta.description = "Latex development environment";
@@ -21,11 +23,13 @@
             
             [ 
               tex 
+              pkgs.inkscape
               pkgs.coreutils
             ];
           nativeBuildInputs = 
             [ 
               tex
+              pkgs.inkscape
               pkgs.just
               pkgs.texmaker
             
@@ -35,6 +39,10 @@
             export PS1='\[\033[1;32m\]\h\[\033[00m\]:\[\033[1;34m\]\w\[\033[00m\] \[\033[38;2;135;0;255m\](nix)\[\033[00m\]\[\033[1;32m\]$(__git_ps1 " (%s)")\[\033[00m\]\$ '
             export OSFONTDIR=".:$OSFONTDIR:"
             export TEXINPUTS=".:$TEXINPUTS:"
+            export XDG_CONFIG_HOME="$PWD/.nix-config/xm1"
+            mkdir -p "$XDG_CONFIG_HOME/xm1"
+            cp -f ${texmakerConfFile} "$XDG_CONFIG_HOME/xm1/texmaker.ini"
+            chmod +w "$XDG_CONFIG_HOME/xm1/texmaker.ini"
           '';
         };
       };
